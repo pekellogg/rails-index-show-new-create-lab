87d062c93dda72efc6592b5b8bed581c8aa87866
Peyton <pekellogg@hotmail.com>
2022-05-03 16:40:26 -0700

    passing tests in spec/*

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: 'ADF411', store: 'Target')
+Coupon.create(coupon_code: 'YSN789', store: 'WallyWorld')
+
+
